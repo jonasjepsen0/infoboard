b73777e444c43608c039a9e2915c08333d2e97f3
phch42 <phch1@elev.techcollege.dk>
2026-03-17 19:46:50 +0100

    refactor(index): mount dashboard to #app and pass element references to initApp

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 // src/index.ts
+
+// Page layout
 import { Dashboard } from './pages/dashboard';
+
+// Application initializer 
 import { initApp } from './main';
 
-// Mount dashboard layout
-const app = Dashboard();
-document.body.appendChild(app);
+// Get root mounting element from index.html
+const root = document.querySelector('#app')!;
 
+// Create dashboard layout + receieve element references
+const { app, refs } = Dashboard();
+
+// Mount application UI
+root.appendChild(app);
 // Initialize dynamic components
-initApp();
+initApp(refs);
