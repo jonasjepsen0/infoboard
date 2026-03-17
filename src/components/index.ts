808f16ba9fe629e17b5187b93474bc2aeda73ba7
phch42 <phch1@elev.techcollege.dk>
2026-03-17 18:32:26 +0100

    refactor(components): add barrel file for cleaner imports

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { initClock } from './clock';
+//export { initWeather } from './weather';
+//export { renderMenu } from './menu';
+//export { renderBuses } from './buses';
+//export { renderActivities } from './activities';
